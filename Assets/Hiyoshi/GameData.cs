d6676279b99d321e4d737c379d55087f42973549
Liuisei <116147262+Liuisei@users.noreply.github.com>
2024-05-22 15:28:47 +0900

    a

--- Assets/Hiyoshi/GameData.cs
+++ Assets/Hiyoshi/GameData.cs
@@ -1,63 +1,22 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using UnityEngine;
 
-public class GameData : MonoBehaviour
+public class DataManager : BaseSingleton<DataManager>
 {
-    public static GameData Instance { get; private set; }
-    protected void Awake()
-    {
-        Instance = this;
-    }
-    List<Ranking> rankings = new List<Ranking>(11);
-    public List<Ranking> rankings_ { get { return rankings; } }
 
-    float        _score;
-    public float Score { get { return _score; } set { _score = value; } }
+    string _playerName;
 
-    void Start()
-    {
-        LoadRankings();
-    }
+    public string PlayerName { get => _playerName; set => _playerName = value; }
 
-    void OnDestroy()
-    {
-        SaveRankings();
-    }
+    protected override void AwakeFunction() { }
 
-    void LoadRankings()
-    {
-        string json = PlayerPrefs.GetString("a");
-        rankings = JsonUtility.FromJson<List<Ranking>>(json);
-    }
 
-    void SaveRankings()
-    {
-        string json = JsonUtility.ToJson(rankings);
-        PlayerPrefs.SetString("a", json);
-    }
 
-    void SortRanking()
+   
+    public void SaveName(string playerName)
     {
-        rankings.OrderBy(e => e.score);
+        PlayerName = playerName;
+        Debug.Log(PlayerName);
     }
-    private void AddRanking(float _score, string _name)
-    {
-        rankings[11].score = _score;
-        rankings[11].name = _name;
-        SortRanking();
-    }
-    
-    
-    
-    
-}
-[Serializable]
-public class Ranking
-{
-    public float score;
-    public string name;
 }
